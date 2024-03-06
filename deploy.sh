bb7ab9dc31fa3cd9f43c1e765e7554418884783d
par0821 <161690752+par0821@users.noreply.github.com>
2024-03-06 15:14:06 +0900

    Update deploy.sh
    addeda  line
    cp CNAME docs/

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 cp CNAME docs/
+cp CNAME content/
 
 # Add changes to git.
 git add .
