c7ca4136b8378d3d04ea7a3aff49bcfbc524f05a
par0821 <161690752+par0821@users.noreply.github.com>
2024-03-06 15:23:25 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-cp CNAME docs/
+cp CNAME content/
 
 # Add changes to git.
 git add .
@@ -17,14 +17,6 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# same on content folder
-cp CNAME content/
-git add .
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
 
 # Push source and build repos.
 git push origin +master
